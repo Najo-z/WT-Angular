683551eabe7efe9282fb3ec10da034d5a4f804cb
clovistricon <clovis.tricon@epitech.eu>
2022-05-03 17:17:27 +0200

    2 files modified

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,41 +5,55 @@ const ObjectID = require('mongodb').ObjectID;
 
 // Connect
 const connection = (closure) => {
-    return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
-        if (err) return console.log(err);
+  return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+    if (err) return console.log(err);
 
-        closure(db);
-    });
+    closure(db);
+  });
 };
 
 // Error handling
 const sendError = (err, res) => {
-    response.status = 501;
-    response.message = typeof err == 'object' ? err.message : err;
-    res.status(501).json(response);
+  response.status = 501;
+  response.message = typeof err == 'object' ? err.message : err;
+  res.status(501).json(response);
 };
 
 // Response handling
 let response = {
-    status: 200,
-    data: [],
-    message: null
+  status: 200,
+  data: [],
+  message: null,
 };
+
+app.put('/signup');
+app.put('/create_note');
+app.put('/:noteId/add_participant');
 
 // Get users
 router.get('/users', (req, res) => {
-    connection((db) => {
-        db.collection('users')
-            .find()
-            .toArray()
-            .then((users) => {
-                response.data = users;
-                res.json(response);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
-    });
+  connection((db) => {
+    db.collection('users')
+      .find()
+      .toArray()
+      .then((users) => {
+        response.data = users;
+        res.json(response);
+      })
+      .catch((err) => {
+        sendError(err, res);
+      });
+  });
 });
+app.get('/tasks');
+app.get('/notes');
+app.get('/:noteId');
+app.get('/:noteId/participants');
+app.get('/:participantId');
+
+app.post('/login');
+app.post('/logout');
+
+app.delete('/:noteId');
 
 module.exports = router;
